a92312e48f1c698ab16dcb8f5f6d0e3006929a4a
Thomas Lieber <tom@alltom.local>
2008-05-20 03:21:53 -0400

    Made first example a little more impressive

--- examples/ex1.rb
+++ examples/ex1.rb
@@ -5,21 +5,25 @@ include UGen
 spork("main") do
 
   wav = WavOut.new("test.wav")
-  SinOsc.new(440, 0.3) >> wav
-  SinOsc.new(880, 0.3) >> wav
+  SinOsc.new(440, 0.25) >> wav
+  SinOsc.new(880, 0.25) >> wav
 
   wav >> blackhole
 
   play 1.second
 
   spork("beep") { beep(wav) }
-
+  
+  play 0.5.seconds
+  
+  spork("beep") { beep(wav) }
+  
   3.times { play 1.seconds }
 
 end
 
 def beep(wav)
-  (s = SawOsc.new(440, 0.3)) >> wav
+  (s = SawOsc.new(440, 0.25)) >> wav
   10.times do
     play 0.1.seconds
     s.freq *= 1.2
