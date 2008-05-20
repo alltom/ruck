9e162e8d3d9d755c79f712bc8c9cb0403e70c73b
Thomas Lieber <tom@alltom.local>
2008-05-20 02:36:08 -0400

    Split Shred and Shreduler into a separate file

--- ruck.rb
+++ ruck.rb
@@ -5,87 +5,6 @@ module Ruck
   SAMPLE_RATE = 44100
   BITS_PER_SAMPLE = 16
   
-  class Shred
-    attr_reader :now
-    attr_accessor :finished
-    
-    def initialize(shreduler, now, name, &block)
-      @shreduler = shreduler
-      @now = now
-      @name = name
-      @block = block
-      @finished = false
-    end
-    
-    def go(resume)
-      @resume = resume
-      @block.call
-      @finished = true
-    end
-    
-    def yield(samples)
-      samples = samples.to_i
-      samples = 0 if samples < 0
-      @now += samples
-      callcc do |cont|
-        @block = cont
-        @resume.call # jump back to shreduler
-      end
-      samples
-    end
-    
-    def <=>(shred)
-      @now <=> shred.now
-    end
-    
-    def to_s
-      "<Shred: #{@name}>"
-    end
-  end
-  
-  class Shreduler
-    attr_reader :running
-    attr_reader :current_shred
-    
-    def initialize
-      @shreds = []
-      @now = 0
-      @running = false
-    end
-    
-    def spork(name, &shred)
-      puts "Adding shred \"#{name}\" at #{@now}"
-      @shreds << Shred.new(self, @now, name, &shred)
-    end
-    
-    def sim
-      min = @shreds.min.now
-      (min - @now).times do
-        dac.next
-        @now += 1
-        puts "#{@now / SAMPLE_RATE} seconds rendered..." if @now % SAMPLE_RATE == 0
-      end
-      @now = min
-    end
-    
-    def run
-      puts "shreduler starting"
-      @running = true
-      
-      while @shreds.length > 0
-        sim
-        @current_shred = @shreds.min
-        callcc { |cont| @current_shred.go(cont) }
-        if @current_shred.finished
-          puts "#{@current_shred} finished"
-          @shreds.delete(@current_shred)
-        end
-      end
-      
-      @running = false
-    end
-  end
-  
   def dac
     @@dac ||= Gain.new
   end
@@ -130,6 +49,7 @@ class Float
 end
 
 require File.join(File.dirname(__FILE__), "linkage")
+require File.join(File.dirname(__FILE__), "shreduling")
 require File.join(File.dirname(__FILE__), "ugen", "general")
 require File.join(File.dirname(__FILE__), "ugen", "wav")
 require File.join(File.dirname(__FILE__), "ugen", "osc")
---
+++ shreduling.rb
@@ -0,0 +1,85 @@
+
+module Ruck
+  
+  class Shred
+    attr_reader :now
+    attr_accessor :finished
+    
+    def initialize(shreduler, now, name, &block)
+      @shreduler = shreduler
+      @now = now
+      @name = name
+      @block = block
+      @finished = false
+    end
+    
+    def go(resume)
+      @resume = resume
+      @block.call
+      @finished = true
+    end
+    
+    def yield(samples)
+      samples = samples.to_i
+      samples = 0 if samples < 0
+      @now += samples
+      callcc do |cont|
+        @block = cont
+        @resume.call # jump back to shreduler
+      end
+      samples
+    end
+    
+    def <=>(shred)
+      @now <=> shred.now
+    end
+    
+    def to_s
+      "<Shred: #{@name}>"
+    end
+  end
+  
+  class Shreduler
+    attr_reader :running
+    attr_reader :current_shred
+    
+    def initialize
+      @shreds = []
+      @now = 0
+      @running = false
+    end
+    
+    def spork(name, &shred)
+      puts "Adding shred \"#{name}\" at #{@now}"
+      @shreds << Shred.new(self, @now, name, &shred)
+    end
+    
+    def sim
+      min = @shreds.min.now
+      (min - @now).times do
+        dac.next
+        @now += 1
+        puts "#{@now / SAMPLE_RATE} seconds rendered..." if @now % SAMPLE_RATE == 0
+      end
+      @now = min
+    end
+    
+    def run
+      puts "shreduler starting"
+      @running = true
+      
+      while @shreds.length > 0
+        sim
+        @current_shred = @shreds.min
+        callcc { |cont| @current_shred.go(cont) }
+        if @current_shred.finished
+          puts "#{@current_shred} finished"
+          @shreds.delete(@current_shred)
+        end
+      end
+      
+      @running = false
+    end
+  end
+  
+end
