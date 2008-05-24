fcb74146847b135b60a4bbb2685fddd48f079fd0
Thomas Lieber <tom@alltom.local>
2008-05-24 15:24:26 -0400

    Oops on linkage

--- ruck.rb
+++ ruck.rb
@@ -56,9 +56,9 @@ class Float
   end
 end
 
-require File.join(File.dirname(__FILE__), "misc", "metaid")
-require File.join(File.dirname(__FILE__), "linkage")
 require File.join(File.dirname(__FILE__), "shreduling")
+require File.join(File.dirname(__FILE__), "misc", "metaid")
+require File.join(File.dirname(__FILE__), "misc", "linkage")
 require File.join(File.dirname(__FILE__), "ugen", "general")
 require File.join(File.dirname(__FILE__), "ugen", "wav")
 require File.join(File.dirname(__FILE__), "ugen", "oscillators")
