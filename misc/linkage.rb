03d934748aa44f830e83bfc5c732b8713a148c2e
Tom Lieber <tom@alltom.com>
2008-05-25 15:23:27 -0400

    Added method so you can tell if an attribute is linked

--- misc/linkage.rb
+++ misc/linkage.rb
@@ -3,8 +3,10 @@ def Object.linkable_attr(attr)
   attr_accessor attr
   define_method("link_#{attr}") do |proc|
     self.metaclass.send(:define_method, attr, &proc)
+    self.metaclass.send(:define_method, "#{attr}_linked?".to_sym) { true }
   end
   define_method("unlink_#{attr}") do
     self.metaclass.send(:define_method, attr) { instance_variable_get("@#{attr}".to_sym) }
+    self.metaclass.send(:define_method, "#{attr}_linked?".to_sym) { false }
   end
 end
