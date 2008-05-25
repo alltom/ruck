be4a7cc0375569a5265405e6716d8be03e8dc408
Tom Lieber <tom@alltom.com>
2008-05-25 03:59:32 -0400

    Split timing helpers into a separate file

--- ruck.rb
+++ ruck.rb
@@ -40,36 +40,16 @@ module Ruck
 
 end
 
-module RuckTime
-  def ms
-    self * Ruck::SAMPLE_RATE / 1000.0
-  end
-  
-  def second
-    self * Ruck::SAMPLE_RATE
-  end
-  alias_method :seconds, :second
-  
-  def minute
-    self * Ruck::SAMPLE_RATE * 60.0
-  end
-  alias_method :minutes, :minute
-end
-
-class Fixnum
-  include RuckTime
-end
-
-class Float
-  include RuckTime
-end
-
 require File.join(File.dirname(__FILE__), "shreduling")
+require File.join(File.dirname(__FILE__), "time")
 require File.join(File.dirname(__FILE__), "misc", "metaid")
 require File.join(File.dirname(__FILE__), "misc", "linkage")
 require File.join(File.dirname(__FILE__), "ugen", "general")
 require File.join(File.dirname(__FILE__), "ugen", "wav")
 require File.join(File.dirname(__FILE__), "ugen", "oscillators")
+
+
+# run the ruck script
 
 unless File.readable?(ARGV[0])
   $stderr.puts "Cannot read file #{ARGV[0]}"
---
+++ time.rb
@@ -0,0 +1,24 @@
+
+module RuckTime
+  def ms
+    self * Ruck::SAMPLE_RATE / 1000.0
+  end
+  
+  def second
+    self * Ruck::SAMPLE_RATE
+  end
+  alias_method :seconds, :second
+  
+  def minute
+    self * Ruck::SAMPLE_RATE * 60.0
+  end
+  alias_method :minutes, :minute
+end
+
+class Fixnum
+  include RuckTime
+end
+
+class Float
+  include RuckTime
+end
