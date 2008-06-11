538ecac04fda6bfe4dec64b423cacbccc8e7940a
Tom Lieber <tom@alltom.com>
2008-06-11 19:36:13 -0400

    Better variable names

--- examples/ex3.rb
+++ examples/ex3.rb
@@ -1,8 +1,8 @@
 wav = WavOut.new(:filename => "ex3.wav")
-s2 = SinOsc.new(:freq => 3)
-s = SinOsc.new(:freq => L{ s2.last * 220 + 660 },
-               :gain => L{ 0.5 + s2.last * 0.5 })
+sin2 = SinOsc.new(:freq => 3)
+sin = SinOsc.new(:freq => L{ sin2.last * 220 + 660 },
+                 :gain => L{ 0.5 + sin2.last * 0.5 })
 
-[s2, s >> wav] >> blackhole
+[sin >> wav, sin2] >> blackhole
 
 play 3.seconds
