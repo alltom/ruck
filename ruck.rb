073e4ab152b3ea19df6b577733ae7981d005aff9
Tom Lieber <tom@alltom.com>
2008-06-13 15:39:06 -0400

    Spork multiple scripts at once

--- ruck.rb
+++ ruck.rb
@@ -6,26 +6,26 @@ module Ruck
 
   SAMPLE_RATE = 22050
   BITS_PER_SAMPLE = 16
-  
+
   def blackhole
     @@blackhole ||= Bus.new
   end
-  
+
   def run
     @shreduler ||= Shreduler.new
     log.error("Ruck already running") and return if @shreduler.running
     @shreduler.run
   end
-  
+
   def spork(name = "unnamed", &shred)
     @shreduler ||= Shreduler.new
     @shreduler.spork(name, &shred)
   end
-  
+
   def play(samples)
     @shreduler.current_shred.yield(samples)
   end
-  
+
   def finish
     shred = @shreduler.current_shred
     @shreduler.remove_shred shred
@@ -43,20 +43,23 @@ require File.join(File.dirname(__FILE__), "ugen", "wav")
 require File.join(File.dirname(__FILE__), "ugen", "oscillators")
 
 
-# run the ruck script
+# run the ruck scripts
 
 if __FILE__ == $0
-  
+
   include Ruck
-  
+
   LOG.level = Logger::WARN
 
-  unless File.readable?(ARGV[0])
-    LOG.fatal "Cannot read file #{ARGV[0]}"
-    exit
+  filenames = ARGV
+  filenames.each do |filename|
+    unless File.readable?(filename)
+      LOG.fatal "Cannot read file #{filename}"
+      exit
+    end
   end
 
-  spork("main") { require ARGV[0] }
+  filenames.each { |filename| spork("main") { require filename } }
   run
-  
+
 end
