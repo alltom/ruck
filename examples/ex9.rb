6fe5c244eb96c78bf0505c6bf8a82fff5918a09f
Tom Lieber <tom@alltom.com>
2008-06-13 21:35:26 -0400

    Updated stereo doubling example

--- examples/ex9.rb
+++ examples/ex9.rb
@@ -6,11 +6,11 @@ https://lists.cs.princeton.edu/pipermail/chuck-users/2008-May/002983.html
 > feed the right channel back to the left with a short delay, inverted;
 =end
 
-wavin = WavIn.new :filename => "widening.wav", :rate => 2.0
+wavin = WavIn.new :filename => "ex1.wav"
 wavout = WavOut.new :filename => "ex9.wav", :num_channels => 2
 
-delayed_left = Delay.new(:time => 10.ms)
-delayed_right = Delay.new(:time => 10.ms)
+delayed_left = Delay.new(:time => 15.ms)
+delayed_right = Delay.new(:time => 15.ms)
 inverted_left = Step.new :value => L{ -delayed_right.next(now) }
 inverted_right = Step.new :value => L{ -delayed_left.next(now) }
 
@@ -18,4 +18,4 @@ wavout >> blackhole
 [wavin.out(0), inverted_right] >> wavout.in(0)
 [wavin.out(1), inverted_left] >> wavout.in(1)
 
-play 3.seconds
+play wavin.duration
