3a28b7ec1f21463f3879bcb45a6dd0b057562a63
Tom Lieber <tom@alltom.com>
2008-06-13 23:17:16 -0400

    Variable naming

--- examples/ex9.rb
+++ examples/ex9.rb
@@ -6,18 +6,18 @@ https://lists.cs.princeton.edu/pipermail/chuck-users/2008-May/002983.html
 > feed the right channel back to the left with a short delay, inverted;
 =end
 
-gain = 0.5
+mix = 0.5
 
 wavin = WavIn.new :filename => "ex1.wav"
 wavout = WavOut.new :filename => "ex9.wav", :num_channels => 2
 
-wavin.out(0) >> (delayed_left =  Delay.new :time => 10.ms, :gain => gain)
-wavin.out(1) >> (delayed_right = Delay.new :time => 10.ms, :gain => gain)
+wavin.out(0) >> (delayed_left =  Delay.new :time => 10.ms, :gain => mix)
+wavin.out(1) >> (delayed_right = Delay.new :time => 10.ms, :gain => mix)
 inverted_left = Step.new :value => L{ -delayed_left.next(now) }
 inverted_right = Step.new :value => L{ -delayed_right.next(now) }
 
 wavout >> blackhole
-[wavin.out(0) * (1.0 - gain), inverted_right] >> wavout.in(0)
-[wavin.out(1) * (1.0 - gain), inverted_left ] >> wavout.in(1)
+[wavin.out(0) * (1.0 - mix), inverted_right] >> wavout.in(0)
+[wavin.out(1) * (1.0 - mix), inverted_left ] >> wavout.in(1)
 
 play wavin.duration
