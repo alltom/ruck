bdbd83f72d4e2cec98a38f3ff015d72f65022902
Tom Lieber <tom@alltom.com>
2008-08-29 00:18:29 +0100

    Updated benchmark script

--- bench.rb
+++ bench.rb
@@ -1,13 +1,42 @@
-TIME = 10 # seconds
-count = 0
-loop do
-  SinOsc.new(:freq => 440) >> blackhole
-  count += 1
-  
-  start = Time.now
-  play TIME.seconds
-  time = Time.now - start
-  puts "at #{count}: #{time}"
-  
-  break if time > TIME
+
+if ARGV.include? "bench.rb"
+  # benchmark UGens with shreduling
+
+  TIME = 1.seconds
+  count = 0
+  puts "Simulating #{TIME / 1.second} seconds"
+  loop do
+    Step.new >> blackhole
+    count += 1
+
+    start = Time.now
+    play TIME
+    time = Time.now - start
+    puts "#{count}: #{time}"
+
+    break if time > (TIME / 1.second)
+  end
+else
+  # benchmark UGens without shreduling
+
+  require "ruck"
+  TIME = 1.seconds
+  dac = Ruck::InChannel.new
+  count = 0
+  @now = 0
+  puts "Simulating #{TIME / 1.second} seconds"
+  loop do
+    Ruck::Step.new >> dac
+    count += 1
+
+    start = Time.now
+    TIME.times do
+      dac.next(@now)
+      @now += 1
+    end
+    time = Time.now - start
+    puts "#{count}: #{time}"
+
+    break if time > (TIME / 1.second)
+  end
 end
