07c9f4050573a49acd05e1bc753de23ee6783b26
Tom Lieber <tom@alltom.com>
2009-11-15 11:29:10 -0800

    made the linkable_attr definition more agreeable with rdoc

--- lib/misc/linkage.rb
+++ lib/misc/linkage.rb
@@ -1,21 +1,21 @@
 
-def Object.linkable_attr(attr)
-  attr_reader attr
-  define_method("#{attr}=") do |val|
-    instance_variable_set("@#{attr}", val)
-    if val.respond_to? :call
-      meta_def attr do
-        val.call
-      end
-    else
-      meta_def attr do
-        val
+class Object
+  def linkable_attr(attr_sym)
+    attr_reader attr_sym
+    define_method("#{attr_sym}=") do |val|
+      instance_variable_set("@#{attr_sym}", val)
+      if val.respond_to? :call
+        meta_def attr_sym do
+          val.call
+        end
+      else
+        meta_def attr_sym do
+          val
+        end
       end
     end
   end
-end
 
-class Object
   def L(&block)
     block
   end
