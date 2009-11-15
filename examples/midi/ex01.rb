ff778263fc3b4253985cf5c24cd7a202d3b39edf
Tom Lieber <tom@alltom.com>
2009-11-14 23:46:46 -0800

    fixed the MIDI example, which also made it more interesting

--- examples/midi/ex01.rb
+++ examples/midi/ex01.rb
@@ -3,14 +3,14 @@ TRACKS[0].name = "My New Track"
 TRACKS[0].instrument = MIDI::GM_PATCH_NAMES[0]
 
 def play(note, dur = 1.quarter_note)
-  note_on 60, 100
+  note_on note, 100
   wait dur
-  note_off 60
+  note_off note
 end
 
 10.times do
-  spork { play(rand(30) + 40) }
-  wait 1.quarter_note
+  spork { play(rand(30) + 40, rand * 1.quarter_note + 1.quarter_note) }
+  wait 0.5.quarter_note
 end
 
 wait 2.quarter_notes; note_off 0 # end padding
