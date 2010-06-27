1353f942f779a7f9e417d81b932a85b2d67c9bc4
Tom Lieber <tom@alltom.com>
2010-06-26 22:14:32 -0700

    greatly improved benchmark readability and usability, even though I'm still probably the only person who'd be interested.

--- lib/ruck/bench.rb
+++ lib/ruck/bench.rb
@@ -1,32 +1,24 @@
 
-SAMPLE_RATE = 44100
+puts "Testing how many Step ugens it takes to synthesize slower than real-time"
 
-if ARGV.include? "bench.rb"
-  # benchmark UGens with shreduling
+seconds = 4
 
-  TIME = 1.seconds
-  count = 0
-  puts "Simulating #{TIME / 1.second} seconds"
-  loop do
-    Step.new >> blackhole
-    count += 1
-
-    start = Time.now
-    play TIME
-    time = Time.now - start
-    puts "#{count}: #{time}"
-
-    break if time > (TIME / 1.second)
-  end
-else
+# were we invoked as a ruck_ugen script, or stand-alone?
+if __FILE__ == $0
+  # we were invoked like "ruby bench.rb"
   # benchmark UGens without shreduling
+  
+  puts "... without shreduling"
 
-  require "../ruck"
-  TIME = SAMPLE_RATE * 4
+  require "ruck"
+  require "ruck/ugen"
+  
+  SAMPLE_RATE = 22050
+  TIME = SAMPLE_RATE * seconds
   dac = Ruck::InChannel.new
   count = 0
   @now = 0
-  puts "Simulating #{TIME / SAMPLE_RATE} seconds"
+  puts "Simulating #{TIME / SAMPLE_RATE} seconds at #{SAMPLE_RATE} sample rate"
   loop do
     Ruck::Generators::Step.new >> dac
     count += 1
@@ -36,9 +28,30 @@ else
       dac.next(@now)
       @now += 1
     end
+    
     time = Time.now - start
-    puts "#{count}: #{time}"
+    puts "#{count} ugens took #{time} sec to synthesize #{TIME / 1.seconds} sec"
 
     break if time > (TIME / SAMPLE_RATE)
+  end
+else
+  # we were invoked by ruck_ugen
+  # benchmark UGens with shreduling
+  
+  puts "... with shreduling"
+
+  TIME = seconds.seconds
+  count = 0
+  puts "Simulating #{TIME / 1.second} seconds at #{SAMPLE_RATE} sample rate"
+  loop do
+    Step.new >> blackhole
+    count += 1
+
+    start = Time.now
+    play TIME
+    time = Time.now - start
+    puts "#{count} ugens took #{time} sec to synthesize #{TIME / 1.seconds} sec"
+
+    break if time > (TIME / 1.second)
   end
 end
