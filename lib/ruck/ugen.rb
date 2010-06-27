0617fce1d271592d77049e5ff5855e634fa1f968
Tom Lieber <tom@alltom.com>
2010-06-26 20:55:51 -0700

    re-extracted time helpers to their own file

--- lib/ruck/ugen.rb
+++ lib/ruck/ugen.rb
@@ -1,4 +1,5 @@
 
 require File.join(File.dirname(__FILE__), "ugen", "ugen")
+require File.join(File.dirname(__FILE__), "ugen", "time_helpers")
 require File.join(File.dirname(__FILE__), "ugen", "oscillators")
 require File.join(File.dirname(__FILE__), "ugen", "wav")
--- lib/ruck/ugen/ugen.rb
+++ lib/ruck/ugen/ugen.rb
@@ -419,33 +419,3 @@ end
 class Array
   include Ruck::Source
 end
-
-# time helpers
-module RuckTime
-  def sample
-    self
-  end
-  alias_method :samples, :sample
-  
-  def ms
-    self.to_f * SAMPLE_RATE / 1000.0
-  end
-  
-  def second
-    self.to_f * SAMPLE_RATE
-  end
-  alias_method :seconds, :second
-  
-  def minute
-    self.to_f * SAMPLE_RATE * 60.0
-  end
-  alias_method :minutes, :minute
-end
-
-class Fixnum
-  include RuckTime
-end
-
-class Float
-  include RuckTime
-end
