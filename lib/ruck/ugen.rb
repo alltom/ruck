cd7506317cb3ae3cccd985b756e588bbd5c64882
Tom Lieber <tom@alltom.com>
2010-06-26 22:13:35 -0700

    forgot a require

--- lib/ruck/ugen.rb
+++ lib/ruck/ugen.rb
@@ -1,5 +1,6 @@
 
 require File.join(File.dirname(__FILE__), "ugen", "ugen")
+require File.join(File.dirname(__FILE__), "ugen", "basic")
 require File.join(File.dirname(__FILE__), "ugen", "time_helpers")
 require File.join(File.dirname(__FILE__), "ugen", "oscillators")
 require File.join(File.dirname(__FILE__), "ugen", "wav")
