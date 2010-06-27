4333ca13238e38684ed1a50e71ab4f52baaf7512
Tom Lieber <tom@alltom.com>
2010-06-26 23:04:51 -0700

    moved ShredLocal module from ruck_realtime to lib/ruck/realtime/realtime.rb. I think I like it there, we'll see. Also added a little explanation for the $. lines at the top of the runners.

--- lib/ruck/realtime/realtime.rb
+++ lib/ruck/realtime/realtime.rb
@@ -1,6 +1,7 @@
 
 module Ruck
   module RealTime
+    
     class RealTimeShreduler < Shreduler
       def run
         @start_time = Time.now
@@ -17,5 +18,27 @@ module Ruck
         @now = new_now
       end
     end
+    
+    # stuff accessible in a shred
+    module ShredLocal
+      def now
+        SHREDULER.now
+      end
+
+      def spork(name = "unnamed", &shred)
+        SHREDULER.spork(name, &shred)
+      end
+
+      def wait(seconds)
+        SHREDULER.current_shred.yield(seconds)
+      end
+
+      def finish
+        shred = SHREDULER.current_shred
+        SHREDULER.remove_shred shred
+        shred.finish
+      end
+    end
+    
   end
 end
